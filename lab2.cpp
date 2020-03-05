eedf5cac39ab4af9269361f943b6c91ed3f8bd63
Ishaadhikary <57455738+Ishaadhikary@users.noreply.github.com>
2020-03-05 10:21:56 +0545

    Create lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+
+#include "List.h"
+
+LinkedList::LinkedList(){
+	HEAD=NULL;
+	TAIL=NULL;
+}
+
+LinkedList::~LinkedList(){
+	
+}
+
+bool LinkedList::isEmpty(){
+	if(HEAD==NULL and TAIL==NULL){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void LinkedList::addToHead(int data){
+	Node* newNode= new Node();
+	newNode->info=data;
+	newNode->next=HEAD;
+	HEAD=newNode;
+	
+	if(TAIL==NULL){
+		TAIL=HEAD;
+	}
+}
+
+void LinkedList::addToTail(int data){
+	Node* newNode= new Node();
+	newNode->info=data;
+	newNode->next=NULL;
+	TAIL->next=newNode;
+	TAIL=newNode;
+}
+
+void LinkedList::add(int data, Node* &predecessor){
+	Node* newNode= new Node();
+	newNode->info=data;
+	newNode->next=predecessor->next;
+	predecessor->next=newNode;
+}
+
+void LinkedList::traverse(){
+	Node* temp=HEAD;
+	while(temp!=NULL){
+		std::cout<<temp->info<<std::endl;;
+		temp=temp->next;
+	}
+}
+
+bool LinkedList::retrieve(int data, Node* &outputptr){
+	Node* temp=HEAD;
+	while(temp!=NULL && temp->info!=data ){
+		temp=temp->next;
+	}
+	if (temp==NULL){
+		return false;
+	}
+	else{
+		outputptr=temp;
+		return true;
+	}
+}
+
+void LinkedList::removeFromHead(){
+	Node* nodeToDelete= new Node();
+	nodeToDelete=HEAD;
+	HEAD=nodeToDelete->next;
+	
+}
+
+void LinkedList::remove(int data){
+	if(isEmpty()){
+		return;
+	}
+	if(HEAD->info=data){
+		removeFromHead();
+		if(HEAD==NULL){
+			TAIL=NULL;
+		}
+	}
+	else{
+		Node* temp= new Node();
+		Node*prev= new Node();
+		temp=HEAD->next;
+		prev=HEAD;
+		
+		while (temp!=NULL){
+			if(temp->info==data){
+				prev->next=temp->next;
+			
+				if(prev->next==NULL){
+				TAIL=prev;
+				}
+			}
+			else{
+				prev=prev->next;
+				temp=temp->next;
+			}
+		}
+	}
+}
+
+int main(){
+	Node *p;
+	LinkedList l;
+	l.addToHead(23);
+	l.addToHead(16);
+	l.addToHead(15);
+  l.addToHead(7);
+  
+	
+	l.retrieve(23,p);
+	l.add(4,p);
+	
+	l.addToTail(8);
+	l.addToTail(9);
+	l.addToTail(18);
+	
+	l.removeFromHead();
+	l.remove(23);
+	
+	l.traverse();
+}
